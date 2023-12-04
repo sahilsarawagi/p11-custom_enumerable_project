2a5344f1da75707efb6c6cf7f76d0dcea0fdf1bd
sahil sarawagi <sahilsarawagi1@gmail.com>
2023-12-04 21:06:46 +0530

    Completed Project: Custom Enumerables All test cases passed

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,57 @@
 module Enumerable
   # Your code goes here
+
+  def my_each_with_index
+    each_with_index {|val,index| yield(val,index)}
+  end
+
+  def my_select
+    ans=[]
+    each {|val| ans.push(val) if yield(val)}
+
+    ans
+  end
+
+  def my_all?
+    each { |num| return false unless yield(num)  }
+
+    true
+  end
+
+  def my_any?
+    each { |num| return true if yield(num)  }
+
+    false
+  end
+  def my_none?
+    each { |num| return false if yield(num)  }
+
+    true
+  end
+
+  def my_count
+    count=0
+    if block_given?
+      each {|num| count+=1 if yield(num)}
+      return count
+    end
+    self.size
+  end
+
+  def my_map
+    ans = []
+    each {|num| ans.push(yield(num))}
+    ans
+  end
+
+  def my_inject(initial_value)
+    each { |num| initial_value = yield(initial_value, num) }
+
+    initial_value
+  end
+  
+
+
 end
 
 # You will first have to define my_each
@@ -8,4 +60,12 @@ end
 # to this method
 class Array
   # Define my_each here
+  
+  def my_each
+    each {|val| yield(val)}
+  end
+
 end
+
+
+
